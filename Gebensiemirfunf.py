80bc28ed4f51b2b6760b28589e07948ef92ebd24
KALILGBADAMOSI <xvkoilsvx@proton.me>
2024-08-05 16:26:04 -0400

    Create Gebensiemirfunf.py

---
+++ Gebensiemirfunf.py
@@ -0,0 +1,20 @@
+x = 5 
+m = 'your excellent number is'
+u = 'now your parabolc number is'
+z = 'x.com/weexcellency'
+
+while x < 624100:
+
+   print( m.title() ,( x * 3 ) ** 2)
+
+   x += 10
+   
+print('OK')
+
+while 624100 < x < 1625625:
+
+   print( u.title() ,( x * 3 ) **4 )
+
+   x += 100 
+
+print('.:!COMPLETE!:.')
